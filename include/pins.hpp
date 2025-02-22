94ab30f32cb6e1c238b40d103be5635746461a20
Heinzino <heinzveintimilla@hotmail.com>
2025-02-21 20:14:40 -0700

    feat: Redefine IOC for new layout

--- include/pins.hpp
+++ include/pins.hpp
@@ -4,25 +4,25 @@
 #include "driver/uart.h"
 
 #if defined(ESP32S3) //PCB / Release Version
-#define BTN1 4
-#define BTN2 5
-#define BTN3 18
-#define BTN4 8
+#define BTN1 14
+#define BTN2 21
+#define BTN3 2
+#define BTN4 1
 
-#define TFT_SCREEN_CS 1
-#define TFT_SCREEN_RESET 2
-#define TFT_SCREEN_DC 42
-#define TFT_SCREEN_LED 41
-#define TFT_CLK 40
-#define TOUCH_SCREEN_CS 39
-#define TFT_MOSI 21
-#define TFT_MISO 14
-#define TOUCH_INTERRUPT_SIGNAL 9
+#define TFT_SCREEN_CS 18
+#define TFT_SCREEN_RESET 8
+#define TFT_SCREEN_DC 9
+#define TFT_SCREEN_LED 10
+#define TFT_CLK 11
+#define TOUCH_SCREEN_CS 12
+#define TFT_MOSI 40
+#define TFT_MISO 38
+#define TOUCH_INTERRUPT_SIGNAL 13
 
-#define SD_CARD_CS 13
-#define SD_CARD_MOSI 12
-#define SD_CARD_MISO 11
-#define SD_CARD_CLK 10
+#define SD_CARD_CS 17
+#define SD_CARD_MOSI 16
+#define SD_CARD_MISO 5
+#define SD_CARD_CLK 4
 
 #define ESP32_UART2_TX 6
 #define ESP32_UART2_RX 7
