110ecb0a27524fac72834d05c94dec96f936c0a1
Heinzino <heinzveintimilla@hotmail.com>
2025-03-30 18:26:39 -0600

    feat: Time progression of song

--- DisplayFirmware/main/Screens/SpotifyScreen.cpp
+++ DisplayFirmware/main/Screens/SpotifyScreen.cpp
@@ -21,6 +21,22 @@ void SpotifyScreen::updateScreen()
     lv_label_set_text(ui_LabelTrack, trackInfo.name.c_str());
     lv_label_set_text(ui_LabelArtist, trackInfo.artistName.c_str());
 
+    currentTrackDuration = trackInfo.duration;
+    currentTrackProgress = trackInfo.progress;
+    if (currentTrackDuration != 0) {
+      lv_bar_set_value(ui_ProgressBar, (currentTrackProgress * 100) / currentTrackDuration, LV_ANIM_OFF);
+    }
+
+    int currentTimeSec = currentTrackProgress / 1000;
+    int totalTimeSec = currentTrackDuration / 1000;
+    char currentTimeStr[16];
+    char totalTimeStr[16];
+    snprintf(currentTimeStr, sizeof(currentTimeStr), "%02d:%02d", currentTimeSec / 60, currentTimeSec % 60);
+    snprintf(totalTimeStr, sizeof(totalTimeStr), "%02d:%02d", totalTimeSec / 60, totalTimeSec % 60);
+    lv_label_set_text(ui_LabelTimeCurrent, currentTimeStr);
+    lv_label_set_text(ui_LabelTimeTotal, totalTimeStr);
+
+
     lv_task_handler();
     lv_timer_handler();
     safeLvglRefresh();
